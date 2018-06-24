a5b0d3946b2ceb38bd34f659e7253815f7366f2e
Chris-Ju <592931812@qq.com>
2018-06-24 20:56:43 +0800

    sql

---
+++ sql.sql
@@ -0,0 +1,87 @@
+create database UMS;
+use UMS;
+
+create table Employee(
+  eid int not null auto_increment,
+  ename char(255) not null,
+  eage int not null,
+  status int not null,
+  esex char(1) default "M" check(sex == "M" or sex == "F"),
+  ehometown char(255),
+  ebody char(255),
+  primary key(id)
+) auto_increment = 1;
+
+create table Department(
+  did int not null auto_increment,
+  dname char(255) not null,
+  dmanager int,
+  foreign key(dmanager) references Employee(eid),
+  primary key(did)
+) auto_increment = 1;
+
+create table EmployeeInDepartment(
+  eid int not null,
+  did int not null,
+  primary key(eid, did),
+  foreign key(eid) references Employee(eid) on delete cascade,
+  foreign key(did) references Department(did) on delete cascade,
+);
+
+create table EmployeeChange(
+  cid int not null auto_increment,
+  eid int not null,
+  did int not null,
+  cway int not null,
+  bdate date not null,
+  edate date,
+  primary key(cid)
+) auto_increment = 1;
+
+create table Contracts(
+  eid int not null,
+  salary int not null,
+  bdate date not null,
+  edate date,
+  primary key(eid, bdate),
+  foreign key(eid) Employee(eid)
+);
+
+create table Attendance(
+  aid int not null auto_increment,
+  eid int not null,
+  away int not null,
+  bdate date not null,
+  edate date,
+  primary key(aid),
+  foreign key(eid) Employee(eid)
+) auto_increment = 1;
+
+create table Rewards(
+  rid int not null auto_increment,
+  eid int not null,
+  rreason char(255) not null,
+  rway int not null,
+  rdate date not null,
+  rmoney int not null
+  primary key(rid),
+  foreign key(eid) references Employee(eid)
+) auto_increment = 1;
+
+create table Training(
+  tid int not null auto_increment,
+  eid int not null,
+  way int not null,
+  bdate date not null,
+  edate date not null,
+  primary key(tid),
+  foreign key(eid) references Employee(eid)
+) auto_increment = 1;
+
+create table User(
+  uid int not null auto_increment,
+  username char(20) not null,
+  password char(255) not null,
+  right int not null,
+  primary key(uid)
+) auto_increment = 1;
