7b98f94cf4b1331fd428c4aa46b1cc67a7c8b0f9
Chris-Ju <592931812@qq.com>
2018-06-30 16:38:22 +0800

    Test

--- model/getAttendance.js
+++ model/getAttendance.js
@@ -1,18 +1,21 @@
 var mysql = require('./connection');
 
 module.exports = () => {
-  mysql.connect();
-  var sql = 'SELECT aid, ename, AW.name, bdate, edate FROM Employee E, Attendance A, AttendanceWay AW where A.away = AW.way and E.eid = A.eid';
-  var data = [];
-  mysql.query(sql, function (err, result) {
-    if (err) {
-      console.log('[QUERY ERROR] - ', err.message);
-      mysql.end();
-      return false;
-    }
-    data = result;
-    console.log('[QUERY Attendance SUCCESSFULLY]');
-  }).then(() => {
+  var promise = new Promise(() => {
+    mysql.connect();
+    var sql = 'SELECT aid, ename, AW.name, bdate, edate FROM Employee E, Attendance A, AttendanceWay AW where A.away = AW.way and E.eid = A.eid';
+    var data = [];
+    mysql.query(sql, function (err, result) {
+      if (err) {
+        console.log('[QUERY ERROR] - ', err.message);
+        mysql.end();
+        return false;
+      }
+      data = result;
+      console.log('[QUERY Attendance SUCCESSFULLY]');
+    })
+  });
+  promise().then(() => {
     mysql.end();
     return data;
   });
--- model/getUsers.js
+++ model/getUsers.js
@@ -2,18 +2,21 @@ var mysql = require('./connection');
 
 
 module.exports = () => {
-  mysql.connect();
-  var sql = 'SELECT username, uright FROM User';
-  var data = [];
-  mysql.query(sql, function (err, result) {
-    if (err) {
-      console.log('[QUERY ERROR] - ', err.message);
-      mysql.end();
-      return false;
-    }
-    data = result;
-    console.log('[QUERY User SUCCESSFULLY]');
-  }).then(() => {
+  var promise = new Promise(() => {
+    mysql.connect();
+    var sql = 'SELECT username, uright FROM User';
+    var data = [];
+    mysql.query(sql, function (err, result) {
+      if (err) {
+        console.log('[QUERY ERROR] - ', err.message);
+        mysql.end();
+        return false;
+      }
+      data = result;
+      console.log('[QUERY User SUCCESSFULLY]');
+    });
+  });
+  promise().then(() => {
     mysql.end();
     return data;
   });
