aaaa8b580ef1b09ea66fb9727d1b112722ef49b0
Chris-Ju <592931812@qq.com>
2018-06-30 17:23:40 +0800

    Test

--- model/getUsers.js
+++ model/getUsers.js
@@ -1,23 +1,19 @@
 var mysql = require('./connection');
 
 module.exports = () => {
-  new Promise(() => {
-      mysql.connect();
-      var sql = 'SELECT username, uright FROM User';
-      var data = [];
-      yield mysql.query(sql, function (err, result) {
-        if (err) {
-          console.log('[QUERY ERROR] - ', err.message);
-          mysql.end();
-          return false;
-        }
-        data = result;
-        console.log('[QUERY User SUCCESSFULLY]');
-      });
-    })
-    .then(() => {
-      console.log(data);
+  mysql.connect();
+  var sql = 'SELECT username, uright FROM User';
+  var data = [];
+  mysql.query(sql, function (err, result) {
+    if (err) {
+      console.log('[QUERY ERROR] - ', err.message);
       mysql.end();
-      return data;
-    });
+      return false;
+    }
+    data = result;
+    console.log('[QUERY User SUCCESSFULLY]');
+    console.log(data);
+    mysql.end();
+    return data;
+  });
 };
