5dbc9bed7d637356c8dc7049da5068eaee4508c3
Chris-Ju <592931812@qq.com>
2018-07-01 12:25:05 +0800

    Done

--- model/getDepartment.js
+++ model/getDepartment.js
@@ -2,7 +2,7 @@ var mysql = require('./connection');
 
 module.exports = (callback) => {
 
-  var sql = 'SELECT dname, dmanager from Department';
+  var sql = 'SELECT did, dname, ename from Department D, Employee E where E.eid = D.dmanager';
   mysql.query(sql, function (err, result) {
     if (err) {
       console.log('[QUERY ERROR] - ', err.message);
--- model/getEmployee.js
+++ model/getEmployee.js
@@ -1,6 +1,6 @@
 var mysql = require('./connection');
 
-module.exports = () => {
+module.exports = (callback) => {
 
   var sql = 'SELECT E.eid, ename, D.dname, ebirth, ES.name, esex, ehometown, ebody FROM Employee E, EmployeeStatus ES, Department D, EmployeeInDepartment ED where E.status = ES.status and E.eid = ED.eid and ED.did = D.did';
   mysql.query(sql, function (err, result) {
