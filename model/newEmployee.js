615ae9e88b6ce6c1425d9a4c26ec06aa0221b1a5
ubuntu <ubuntu@localhost.localdomain>
2018-07-06 11:38:16 +0800

    newEmployee

--- model/newEmployee.js
+++ model/newEmployee.js
@@ -1,6 +1,5 @@
 var mysql = require('./connection');
 var md5 = require('md5');
-var pinyin = require('js-pinyin');
 
 module.exports = (name, birth, sex, hometown, body, department, callback) => {
 
@@ -15,12 +14,12 @@ module.exports = (name, birth, sex, hometown, body, department, callback) => {
     console.log('[INSERT INTO Employee SUCCESSFULLY]');
   });
 
-  sql = 'INSERT INTO EmployeeChange(eid, cdate, tdid) ((SELECT eid, NOW(), did FROM (SELECT eid, did, dname FROM Employee JOIN Department) AS tb WHERE tb.dname = ? ORDER BY tb.eid DESC LIMIT 1))';
+  sql = 'INSERT INTO EmployeeChange(eid, cdate, tdid) (SELECT eid, NOW(), did FROM (SELECT eid, did, dname FROM Employee JOIN Department) AS tb WHERE tb.dname = ? ORDER BY tb.eid DESC LIMIT 1)';
   sqlParams = [department];
-  msql.query(sql, sqlParams, function (err, result) {
+  mysql.query(sql, sqlParams, function (err, result) {
     if (err) {
       console.log('[INSERT ERROR] - ', err.message);
-      msql.end();
+      mysql.end();
       callback(false);
     }
     console.log('[INSERT EmployeeChange SUCCESSFULLY]');
@@ -38,14 +37,9 @@ module.exports = (name, birth, sex, hometown, body, department, callback) => {
       console.log('[INSERT INTO EmployeeInDepartment SUCCESSFULLY]');
     });
   }
-
-  pinyin.setOptions({
-    checkPolyphone: false,
-    charCase: 0
-  });
   
   sql = 'INSERT INTO User(username, password) VALUE(?, ?)';
-  var username = pinyin.getFullChars(name);
+  var username = name;
   var password = md5("123456");
   console.log(username);
   sqlparamas = [username, password];
