b5b4a6d11fcd327c9093e58b6f05c3cf9e1369c7
ubuntu <ubuntu@localhost.localdomain>
2018-07-06 13:20:34 +0800

    FUCK

--- model/getDepartment.js
+++ model/getDepartment.js
@@ -2,7 +2,7 @@ var mysql = require('./connection');
 
 module.exports = (callback) => {
 
-  var sql = 'SELECT did, dname, ename from Department D, Employee E where E.eid = D.dmanager';
+  var sql = "SELECT did, dname, ename from Department D, Employee E where E.eid = D.dmanager or (D.dmanager is null and E.eid = 0 and D.did != 0) ORDER BY did"
   mysql.query(sql, function (err, result) {
     if (err) {
       console.log('[QUERY ERROR] - ', err.message);
