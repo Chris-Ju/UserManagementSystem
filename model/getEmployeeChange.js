7cca1cc5f93a803afe0e1af548826189a2031ed4
Chris-Ju <592931812@qq.com>
2018-07-06 15:44:23 +0800

    sbug

--- model/getEmployeeChange.js
+++ model/getEmployeeChange.js
@@ -2,7 +2,7 @@ var mysql = require('./connection');
 
 module.exports = (callback) => {
 
-  var sql = 'SELECT cid, E.ename, CW.name, cdate, D1.dname tname, D2.fname tname FROM Employee E, EmployeeChange C, EmployeeChangeWay CW, Department D1, Department D2 WHERE C.cway = CW.way and E.eid = C.eid and (C.tdid = D1.did or C.tdid is null and D1.did = 0) and (D2.did = 0 and C.fdid is null or D2.did = C.fdid) ORDER BY cid';
+  var sql = 'SELECT cid, E.ename, CW.name, cdate, D1.dname tname, D2.dname fname FROM Employee E, EmployeeChange C, EmployeeChangeWay CW, Department D1, Department D2 WHERE C.cway = CW.way and E.eid = C.eid and (C.tdid = D1.did or C.tdid is null and D1.did = 0) and (D2.did = 0 and C.fdid is null or D2.did = C.fdid) ORDER BY cid';
   mysql.query(sql, function (err, result) {
     if (err) {
       console.log('[QUERY ERROR] - ', err.message);
