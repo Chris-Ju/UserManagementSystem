3bb14c3ceabc0f9275ec0be9bc39d5ca207a146c
Chris-Ju <592931812@qq.com>
2018-07-06 11:40:26 +0800

    callback

--- model/newEmployee.js
+++ model/newEmployee.js
@@ -8,50 +8,52 @@ module.exports = (name, birth, sex, hometown, body, department, callback) => {
   mysql.query(sql, sqlParams, function (err, result) {
     if (err) {
       console.log('[INSERT ERROR] - ', err.message);
-      
+
       callback(false);
     }
     console.log('[INSERT INTO Employee SUCCESSFULLY]');
-  });
 
-  sql = 'INSERT INTO EmployeeChange(eid, cdate, tdid) (SELECT eid, NOW(), did FROM (SELECT eid, did, dname FROM Employee JOIN Department) AS tb WHERE tb.dname = ? ORDER BY tb.eid DESC LIMIT 1)';
-  sqlParams = [department];
-  mysql.query(sql, sqlParams, function (err, result) {
-    if (err) {
-      console.log('[INSERT ERROR] - ', err.message);
-      mysql.end();
-      callback(false);
-    }
-    console.log('[INSERT EmployeeChange SUCCESSFULLY]');
-  });
 
-  sql = 'INSERT INTO EmployeeInDepartment (SELECT eid, did FROM (SELECT eid, did, dname FROM Employee JOIN Department) AS tb WHERE tb.dname = ? ORDER BY tb.eid DESC LIMIT 1)';
-  sqlparamas = [department];
-  if (department != "") {
+    sql = 'INSERT INTO EmployeeChange(eid, cdate, tdid) (SELECT eid, NOW(), did FROM (SELECT eid, did, dname FROM Employee JOIN Department) AS tb WHERE tb.dname = ? ORDER BY tb.eid DESC LIMIT 1)';
+    sqlParams = [department];
     mysql.query(sql, sqlParams, function (err, result) {
       if (err) {
         console.log('[INSERT ERROR] - ', err.message);
-        
+        mysql.end();
         callback(false);
       }
-      console.log('[INSERT INTO EmployeeInDepartment SUCCESSFULLY]');
-    });
-  }
-  
-  sql = 'INSERT INTO User(username, password) VALUE(?, ?)';
-  var username = name;
-  var password = md5("123456");
-  console.log(username);
-  sqlparamas = [username, password];
-  mysql.query(sql, sqlParams, function (err, result) {
-    if (err) {
-      console.log('[INSERT ERROR] - ', err.message);
-      
-      callback(false);
-    }
-    console.log('[INSERT INTO User SUCCESSFULLY]');
-    
-    callback(true);
-  });
+      console.log('[INSERT EmployeeChange SUCCESSFULLY]');
 
+
+      sql = 'INSERT INTO EmployeeInDepartment (SELECT eid, did FROM (SELECT eid, did, dname FROM Employee JOIN Department) AS tb WHERE tb.dname = ? ORDER BY tb.eid DESC LIMIT 1)';
+      sqlparamas = [department];
+      if (department != "") {
+        mysql.query(sql, sqlParams, function (err, result) {
+          if (err) {
+            console.log('[INSERT ERROR] - ', err.message);
+
+            callback(false);
+          }
+          console.log('[INSERT INTO EmployeeInDepartment SUCCESSFULLY]');
+
+          
+          sql = 'INSERT INTO User(username, password) VALUE(?, ?)';
+          var username = name;
+          var password = md5("123456");
+          console.log(username);
+          sqlparamas = [username, password];
+          mysql.query(sql, sqlParams, function (err, result) {
+            if (err) {
+              console.log('[INSERT ERROR] - ', err.message);
+
+              callback(false);
+            }
+            console.log('[INSERT INTO User SUCCESSFULLY]');
+
+            callback(true);
+          });
+        });
+      }
+    });
+  });
 };
